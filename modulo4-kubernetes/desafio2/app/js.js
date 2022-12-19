d9699f647648fc51ed5239c714ba6f0353f381e2
robson-tazinaffo <robsontazinaffo@gmail.com>
2022-12-19 20:01:27 -0300

    Criando um pipeline de deploy com GitLab e Kubernetes

---
+++ modulo4-kubernetes/desafio2/app/js.js
@@ -0,0 +1,15 @@
+$('#btn_gravar').on('click', function () {
+    var txt_nome = $('#nome').val()
+    var txt_mensagem = $('#mensagem').val()
+
+    $.ajax({
+        url: 'http://192.168.150.129/incluir.php',
+        type: 'post',
+        data: { nome: txt_nome, mensagem: txt_mensagem },
+        beforeSend: function () {
+            $('#resposta').html('Enviando......')
+        }
+    }).done(function (e) {
+        $('#resposta').html('Dados salvos......')
+    })
+})
---
+++ modulo4-kubernetes/desafio2/mysql/sql.sql
@@ -0,0 +1,5 @@
+CREATE TABLE mensagens (
+    id int,
+    nome varchar(60),
+    mensagem varchar(100)
+);
